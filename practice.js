869881267fe784ae656ee3ef4e79124bf2843360
Saikat Ghosh <saikat.blast20@gmail.com>
2025-03-07 03:01:43 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,3 +1,36 @@
-document.getElementById('moveButton').addEventListener('click', function() {
-    document.getElementById('box').style.transform = 'translate(150px, 150px)';
-});
+// document.getElementById('moveButton').addEventListener('click', function() {
+//     document.getElementById('box').style.transform = 'translate(150px, 150px)';
+// });
+
+// get method 
+
+// a getter is a method that retrives the value of poperty 
+// it allows you to access a property like a variable instead of calling a function 
+// const user = {
+//     firstName: "saikat",
+//     lastName: "Ghosh",
+
+//     get fullName(){
+//         return this.firstName + " " + this .lastName;
+//     }
+// };
+
+
+// console.log(user.fullName);
+
+// if we didn't use above code then the code will look like this 
+// const user = {
+//     firstName: "saikat",
+//     lastName: "Ghosh",
+
+//     fullName: function (){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(user.fullName());
+
+
+// set method: 
+
+
