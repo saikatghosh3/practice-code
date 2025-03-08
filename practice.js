41c79e0bd827f7932908de91f86cd441cc5ad86d
Saikat Ghosh <saikat.blast20@gmail.com>
2025-03-09 03:24:28 +0600

    practice

--- practice.js
+++ practice.js
@@ -32,5 +32,48 @@
 
 
 // set method: 
+// A setter is used to update a property's value
+// it allows validation or modification of the value before updation the property 
 
 
+// const  user ={
+//     firstName: "saikat", 
+//     lastName: "Ghosh", 
+//     get fullName(){
+//         return this.firstName + "" + this .lastName;
+//     },
+//     set fullName(newName) {
+//         const parts = newName.split(" ");
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+
+//         // updating the name
+//         user.fullName = "jhon Doe";
+//         console.log(user.firstName);
+//         console.log(user.lastName);
+//     }
+// }
+
+// oject and class understanding.
+// class:
+
+class ToyotaCar {
+    start(){
+        console.log("start")
+    }
+    stop(){
+        console.log("stop");
+    }
+
+    setBrand(brand){
+        this.brand =brand;
+        // jei ta this er sathe ache oita object er property.
+        // ar onno ta jeita parameter akare ache ba argument akare asbe 
+    }
+}
+
+let fortuner = new ToyotaCar();
+fortuner.setBrand("fortuner");
+let lexus = ToyotaCar();
+
+// when we need multiple object based on same template then we prefer class 
