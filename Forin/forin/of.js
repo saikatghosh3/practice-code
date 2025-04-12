db3fd48dca2cda2df122734a7b49c3a73ba319dc
Saikat Ghosh <saikat.blast20@gmail.com>
2025-04-12 18:59:01 +0600

    basic practice

--- Forin/forin/of.js
+++ Forin/forin/of.js
@@ -135,3 +135,141 @@ function upload(file){
 }
 
 uploadFile({ name: "image.png", size: 6000000, type: "image/png" });
+
+
+//  Given an array of numbers, find the total sum.
+
+let numbers = [5,10,15, 20];
+
+function sumArray(arr){
+    let total = 0;
+    for(let i= 0; i<arr.length; i++);{
+    total += arr[i];
+}
+return total;
+}
+console.log(sumArray([4,5,6,7,8,9,10]));
+
+// count even and odd in the array are even vs odd.
+
+function countEvenOdd(arr){
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i<arr.length; i++){
+        if(arr[i] % 2 === 0){
+            even ++;
+        }else {
+            odd++;
+        }
+    } 
+    console.log(`Even: ${even}, Odd: ${odd}`)
+    
+}
+
+
+// some problem based on  || and && operater 
+// && condition : requirements
+// if both A and B are true, return true;
+// if either is false return false;
+
+// || condtion :
+// if either A or B is true, return true;
+// only returns false if both are false;
+
+
+
+
+// check a person is 18 and has license
+// let age = 19;
+// let hasLicense = true;
+// if(age >=18 && hasLicense){
+//     console.log("you can drive");
+// }
+
+// accessing with || operator 
+
+let isLoggedIn = false;
+let isGuest = true;
+
+if(isLoggedIn || isGuest){
+    console.log("Access granted");
+}
+
+
+// Login Auth with && 
+let enteredUsername = "admin";
+let enterdPassword = "1234";
+
+if (enteredUsername === "admin" && enterdPassword === "password");{
+    console.log("Login successful");
+}
+
+
+
+// nested loop problems 
+
+// print 4 stars in 3 rows 
+function PrintStars(rows, cols){
+   
+    for(i = 1; i<=rows; i++){
+        line = ")";
+        for(j= 1; j<= cols; j++){
+            line += "*";
+        }
+        console.log(line)
+    }
+}
+console.log(PrintStars(3,4));
+
+
+// Number pattern (triangle)
+
+function printNumberTriangle(n){
+    for(let i = 1; i<= n ; i++){
+        let line = "";
+        for(j= 1; j<=i; j++){
+            line +=j + "";
+        }
+        console.log(line);
+    }
+}
+
+printNumberTriangle(n);
+
+
+// some real life example
+
+// a restaurant wants to show all possible combinations of means and drinks
+
+let meals = [ "burger", "Pizza", "pasta"];
+let drinks = ["coke", "water", "juice"];
+for(let i = 0; i<meals.length; i++){
+    for(let j= 0; j<drinks.length; j++){
+        console.log(`${meals[i]} with ${drinks[j]}`);
+    }
+}
+
+// In a school, every student has to take every subject.
+//  Print what subject each student is taking.
+
+let students = ["Bob", "Jhon", "Peter"];
+let subjects = ["Bangla", "math", "social-science"];
+
+for(i= 0; i<subjects.length; i++){
+    for(j = 0; j< subjects.length; j++){
+        console.log(`${students[i]} studies ${subjects[j]}`);
+    }
+}
+
+// ask the user how many rows and seats are in a tehater.
+let rows = parseInt(prompt("Enter a number of rows :"));
+let seats = parseInt(prompt("Enter the number seats per row :"));
+
+for(let i= 1; i<= rows; i++){
+    let seatRow = `Row ${i}`;
+    for (j=1; j<= seats; j++){
+        seatRow += `[${j}]`;
+    }
+    console.log(seatRow);
+}
