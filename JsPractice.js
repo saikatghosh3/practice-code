aa4f278428d24a18203319abaebc2199c4db7db1
Saikat Ghosh <saikat.blast20@gmail.com>
2025-08-16 23:15:51 +0600

    basic-practice

--- JsPractice.js
+++ JsPractice.js
@@ -45,3 +45,80 @@ function sayHello(){
     console.log('Hello world');
 }
 sayHello();
+
+
+// object literal 
+
+// key value pair er maddhome acceess korte hoi 
+
+let person = {
+    name: "saikat", 
+    age : 28,
+    location : {
+        city: 'jashore',
+        zip: 7400,
+    },
+    sayHello: function(){
+        console.log('hello saikat');
+    },
+};
+
+console.log(person);
+console.log(person.location.city);
+console.log(person['location']['city']);
+
+person.sayHello();
+// this one is method
+// means person  is a object and inside that there is a method name sayHello();
+// similarly Math.random()
+
+
+// more properties and method 
+
+// now we'll try reassign value 
+
+let person = {
+    name: 'saikat',
+    age: 28,
+    sayHello: function(){
+        console.log('hello world')
+    }
+};
+
+person.name = "ghosh";
+console.log(person.name);
+
+// now we'll assign a new property
+
+person.age = 30;
+console.log(person);
+
+// for deleting 
+
+delete person.age;
+console.log(person);
+
+// now we'll store the value
+
+let fullName = person.name;
+console.log(fullName);
+
+
+// method 
+person.sayHello();
+
+// creating method to original object
+
+person.add = function(a,b){
+    return a +b;
+}
+
+ console.log(person.add(5,6));
+
+//  another way 
+
+console.log(person['add'](5,4));
+
+
+
+
